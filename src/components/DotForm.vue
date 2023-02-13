5f924e87fa406a2e9619ea1e0bd11873d1037f49
ThisAster <338879@niuitmo.ru>
2023-02-13 15:02:10 +0300

    create project

---
+++ src/components/DotForm.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "DotForm"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ResultTable.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+name: "ResultTable"
+}
+</script>
+
+<style scoped>
+
+</style>
