2c90deac7689b34df23062af0464f9b993a9357a
ThisAster <338879@niuitmo.ru>
2023-02-20 00:26:10 +0300

    edit

--- src/components/MainPage/MainPageComponents/ResultTable.vue
+++ src/components/MainPage/MainPageComponents/ResultTable.vue
@@ -13,7 +13,7 @@
                 <td>{{ row.X }}</td>
                 <td>{{ row.Y }}</td>
                 <td>{{ row.R }}</td>
-                <td>{{ getUtcOffset(new Date(parseInt(row.attemptTime).toLocaleString)) }}</td>
+                <td>{{ timeChanger(new Date(parseInt(row.attemptTime).toLocaleString)) }}</td>
                 <td>{{ (parseInt(row.processTime)/1000/1000).toFixed(2)  }} ms</td>
                 <td>{{ row.result }}</td>
             </tr>
@@ -27,7 +27,7 @@ import moment from 'moment';
 export default {
   props: ["allTableRows"],
   methods: {
-    getUtcOffset(date) {
+    timeChanger(date) {
         return moment(date)
                 .subtract(
                 moment(date).utcOffset(), 
